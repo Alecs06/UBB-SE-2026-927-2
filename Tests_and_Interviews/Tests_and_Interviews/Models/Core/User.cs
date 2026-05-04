2b4cab7c1bd67b19c85749eecfe81ba2d6d0cbd2
maiaiia <maiaaungureanu@gmail.com>
2026-05-04 14:43:21 +0300

    fix data annotations for core domain classes

--- Tests_and_Interviews/Tests_and_Interviews/Models/Core/User.cs
+++ Tests_and_Interviews/Tests_and_Interviews/Models/Core/User.cs
@@ -1,17 +1,21 @@
 ﻿namespace Tests_and_Interviews.Models.Core
 {
-    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
+    /// <summary>
+    /// Represents an User.
+    /// </summary>
+    [Table("Users")]
     public class User
     {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-
-        public string Email { get; set; }
-
-        public string? CvXml { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
+        /// <param name="id">The user's unique identifier.</param>
+        /// <param name="name">The user's name.</param>
+        /// <param name="email">The user's email.</param>
+        /// <param name="cvXml">The user's cv in xml format.</param>
         public User(int id, string name, string email, string? cvXml = null)
         {
             this.Id = id;
@@ -19,5 +23,39 @@
             this.Email = email;
             this.CvXml = cvXml;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// Parameterless constructor used by EF Core, hence the protected status.
+        /// </summary>
+        protected User()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the User.
+        /// </summary>
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's name.
+        /// </summary>
+        [Column("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's email.
+        /// </summary
+        [Column("email")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's cv in xml format.
+        /// </summary>
+        [Column("cv_xml")]
+        public string? CvXml { get; set; }
     }
 }
