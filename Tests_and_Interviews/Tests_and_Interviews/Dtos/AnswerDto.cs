8b68aa8b68f70ed956eb2fa5452716dd7c92c33d
V-Alexandra <alexandra.vadan@stud.ubbcluj.ro>
2026-05-05 20:35:14 +0300

    Update AnswerDto.cs

--- Tests_and_Interviews/Tests_and_Interviews/Dtos/AnswerDto.cs
+++ Tests_and_Interviews/Tests_and_Interviews/Dtos/AnswerDto.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Tests_and_Interviews.Dtos
 {
-    internal class AnswerDto
+    /// <summary>
+    /// DTO representing an answer to a question in a test attempt. It contains the ID of the question being answered and the value of
+    /// the answer provided by the user. This DTO is used to transfer answer data between the client and server when submitting test
+    /// attempts or retrieving test results.
+    /// </summary>
+    public class AnswerDto
     {
+        public int QuestionId { get; set; }
+
+        public string Value { get; set; } = string.Empty;
     }
 }
